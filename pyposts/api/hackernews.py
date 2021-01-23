82c2fff3b9acc718b28e89083b036db9d65d7011
Damian Tabor <zettie008@gmail.com>
2021-01-23 16:15:26 +0000

    Added function for fetching first page of HN and displaying content using Parser

--- pyposts/api/hackernews.py
+++ pyposts/api/hackernews.py
@@ -1,5 +1,8 @@
+from typing import Generator
+
 import click
 import requests
+from bs4 import BeautifulSoup as BS
 
 
 @click.command("hnews", short_help="Get latest news from Hackernews")
@@ -7,4 +10,20 @@ def hnews() -> None:
     """
     Fetch the latest from HackerNews!
     """
-    print("Working")
+
+    click.echo_via_pager(fetch_news())
+
+def fetch_news() -> Generator[str, None, None]:
+    """
+    Display all posts from the given page
+    """
+    URL = "https://news.ycombinator.com/"
+    response = requests.get(URL)
+
+    page = BS(response.content, "html.parser")
+    stories = page.find_all("a", class_="storylink")
+    for story in stories:
+        link = story.get("href", "")
+        text = story.text.strip()
+        yield text + "\n" * 2
+
