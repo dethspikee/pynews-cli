586f39fb558ab26023dea5f3fd7b218cc3bab26c
Damian Tabor <zettie008@gmail.com>
2021-01-23 17:21:12 +0000

    Changed exit codes from 1 to 2 on exceptions

--- pyposts/exceptions.py
+++ pyposts/exceptions.py
@@ -12,27 +12,27 @@ def check_for_request_errors(func):
             print("Cannot establish connection")
             print("Full error message: ")
             print(e)
-            sys.exit(1)
+            sys.exit(2)
         except requests.exceptions.Timeout as e:
             print("Request timed out")
             print("Full error message: ")
             print(e)
-            sys.exit(1)
+            sys.exit(2)
         except requests.exceptions.HTTPError as e:
             print("An HTTP error occurred")
             print("Full error message: ")
             print(e)
-            sys.exit(1)
+            sys.exit(2)
         except requests.exceptions.RequestException as e:
             print("An SSL error occurred")
             print("Full error message: ")
             print(e)
-            sys.exit(1)
+            sys.exit(2)
         except json.JSONDecodeError as e:
             print("Something went wrong. Check your request")
             print("Full error message: ")
             print(e)
-            sys.exit(1)
+            sys.exit(2)
 
     return wrapper
 
