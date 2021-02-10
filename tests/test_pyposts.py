d31b6c66862ff26f663edb504b00a4f8f59a4a15
Damian Tabor <zettie008@gmail.com>
2021-02-10 21:46:21 +0000

    Add test for pynews main menu

--- tests/test_pyposts.py
+++ tests/test_pyposts.py
@@ -1,5 +1,16 @@
+from click.testing import CliRunner
+
 from pynews import __version__
+from pynews.cli import cli
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_pynews_cli():
+    runner = CliRunner()
+    result = runner.invoke(cli)
+
+    assert "hnews" in result.output
+    assert result.exit_code == 0
